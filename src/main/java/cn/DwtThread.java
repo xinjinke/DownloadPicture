9feaad47640a63211a10e0dd3658c65eef24115d
xinjinke <1649535910@qq.com>
2018-04-02 22:38:40 +0800

    修改502，延时重试

--- src/main/java/cn/DwtThread.java
+++ src/main/java/cn/DwtThread.java
@@ -153,11 +153,11 @@ public class DwtThread extends Thread{
 
                     while (iterator.hasNext()) {
 
-//                        try {
-//                            Thread.currentThread().sleep(200);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
+                        try {
+                            Thread.currentThread().sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         final String postUrl = (String) iterator.next();
 
                         Thread thread = new Thread(new Runnable() {
@@ -181,6 +181,15 @@ public class DwtThread extends Thread{
                         break;
                     }
                 }
+            }
+
+            if(i % 6 ==0){
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("已下载6个帖子");
             }
         }
        logger.info("\n" + "**************本次下载"+Url+"完成" + sdf2.format(new Date()) + "**************************************************\n");
@@ -243,7 +252,17 @@ public class DwtThread extends Thread{
         try {
             doc = Jsoup.connect(url).userAgent(UA).get();
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.out.println("被封，休息0.4秒后再试");
+        }
+
+        while(doc == null){
+            try{
+                Thread.sleep(400);
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+            doc = getDocument(url);
         }
         return doc;
     }
@@ -251,6 +270,7 @@ public class DwtThread extends Thread{
 
     public static int setPageSize(String url) {
         Document doc = getDocument(url);
+
         Elements es = doc.select("div.pages a"); //page a
         int pageSize = 1;
         for (Iterator<Element> i = es.iterator(); i.hasNext();) {
